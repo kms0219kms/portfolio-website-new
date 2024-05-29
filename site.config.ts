6790f0e6387e0b23e6cd81ac9955cfcd6ea92648
Minsu Kim <minsu_kim@bishanoi.net>
2024-05-29 21:52:41 +0700

    :rocket: feat: Setting up config file

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1e89e9e42eb4ebf9486ae0374039efc',
+  rootNotionPageId: '087bf8634f4d4082ae987c23c82fc42c',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: '2skydev blog',
-  domain: 'blog.2skydev.com',
-  author: '2skydev',
+  name: 'Hello, I’m Minsu Kim.',
+  domain: 'devayaan.me',
+  author: 'Minsu Kim',
 
   // open graph metadata (optional)
-  description: '2skydev blog - developer blog',
+  description: 'Hello, I’m Minsu Kim.',
 
   // social usernames (optional)
-  // twitter: 'transitive_bs',
-  github: '2skydev',
-  // linkedin: 'fisch2',
+  twitter: 'kms0219kms',
+  github: 'kms0219kms',
+  linkedin: 'kms0219kms',
   // newsletter: '#', // optional newsletter URL
-  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+  youtube: 'UCTKVrq3rLLyWWkhTPVbKpag', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon:
-    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0d2daa37-61d0-45b6-b333-9a2bd0bdc3ee%2Fprofile_%25E1%2584%2580%25E1%2585%25A9%25E1%2584%2592%25E1%2585%25AA%25E1%2584%258C%25E1%2585%25B5%25E1%2586%25AF_circle.png?table=block&id=d1e89e9e-42eb-4ebf-9486-ae0374039efc&spaceId=2eb5336b-2edb-42d0-bc6c-95d72d4d1b74&width=250&userId=bef10e95-202b-4b6b-9626-7af866b6f9ba&cache=v2',
+    'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F20b38eb8-84d2-4216-9c70-8f45c1e8515d%2F%25EA%25B7%25B8%25EB%25A6%25BC1.png?table=block&id=087bf863-4f4d-4082-ae98-7c23c82fc42c&spaceId=d85e8adb-976a-46f1-8de8-5e8516d5f528&width=250&userId=ff9eeeea-cd92-43af-b243-ff264d4520bf&cache=v2',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -46,22 +46,27 @@ export default siteConfig({
   //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
   //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
   // }
-  pageUrlOverrides: null,
+  pageUrlOverrides: {
+    '/blog-ko': '9ef021736d054df5b671090442e5ead7',
+    '/resume': '3a39eafc55dc44d799f39e530fd4328c',
+    '/ko': '087bf8634f4d4082ae987c23c82fc42c',
+    '/en': '1f17207bf68a41cb9aeb049b270415ff'
+  },
 
   // whether to use the default notion navigation style or a custom one with links to
-  // important pages
-  navigationStyle: 'custom',
-  navigationLinks: [
-    {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
-    },
-    {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
-      menuPage: true,
-    },
-  ],
+  // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
+  navigationStyle: 'default',
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
 
   // -------- custom configs (2skydev) -------------
 
